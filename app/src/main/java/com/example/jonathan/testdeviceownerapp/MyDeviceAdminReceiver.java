15d580bf7154789a2ffaf011fda5263d9b71eec6
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2018-12-20 11:50:58 -0800

    Store the APK for download later on during the provisioing process.

--- app/src/main/java/com/example/jonathan/testdeviceownerapp/MyDeviceAdminReceiver.java
+++ app/src/main/java/com/example/jonathan/testdeviceownerapp/MyDeviceAdminReceiver.java
@@ -11,10 +11,28 @@ public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
   private static final String TAG = ConstUtils.APP_TAG +
       MyDeviceAdminReceiver.class.getSimpleName();
 
+  /**
+   * Intercept standard device administrator broadcasts.  Implementations
+   * should not override this method; it is better to implement the
+   * convenience callbacks for each action.
+   */
   @Override
   public void onReceive(Context context, Intent intent) {
     Log.d(TAG, "onReceive: context=[" + context +
         "], intent.action=[" + intent.getAction() + "]");
+
+    super.onReceive(context, intent);
+
+    // TODO
+  }
+
+  @Override
+  public void onEnabled(Context context, Intent intent) {
+    Log.d(TAG, "onEnabled: context=[" + context +
+        "], intent.action=[" + intent.getAction() + "]");
+
+    super.onEnabled(context, intent);
+
     // TODO
   }
 }
